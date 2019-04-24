1cdf3e22bddea55750458bca36faf5cb410c21c4
Nishant Arora <nish54@gmail.com>
2019-04-24 16:51:55 -0400

    added empty single view app

---
+++ planner/planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  planner
+//
+//  Created by Nishant Arora on 4/24/19.
+//  Copyright © 2019 Nishant Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
