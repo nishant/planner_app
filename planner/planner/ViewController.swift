eea4e25325c9d253d4692d6640bef0b0e40f2992
Nishant Arora <nish54@gmail.com>
2019-05-03 15:34:19 -0400

    added table view controller for assignment tracker

--- planner/planner/ViewController.swift
+++ planner/planner/ViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var assignmentButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func loadAssignmentView(_ sender: Any) {
+        performSegue(withIdentifier: "toAssignmentView", sender: self)
+    }
+    
 }
 
