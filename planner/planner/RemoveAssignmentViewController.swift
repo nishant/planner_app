63ff2cda60d80a0424f115b0e049c2a7b7467c66
Nishant Arora <nish54@gmail.com>
2019-05-11 06:49:48 -0400

    working, no core data yet

---
+++ planner/planner/RemoveAssignmentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RemoveAssignmentViewController.swift
+//  planner
+//
+//  Created by Nishant Arora on 5/11/19.
+//  Copyright © 2019 Nishant Arora. All rights reserved.
+//
+
+import UIKit
+
+class RemoveAssignmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
